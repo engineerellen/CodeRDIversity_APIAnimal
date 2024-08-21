9cff2e739ab34778f0bee859f0abb28f12106db2
Ellen <36012338+engineerellen@users.noreply.github.com>
2024-08-20 22:26:58 -0300

    criacao da console

---
+++ ConsoleAnimal/Program.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+
+var bebe = new Humano(1, "Noah", 3, "meses");
+Console.WriteLine(bebe.Nascer());
+Console.WriteLine(bebe.Dormir());
+
+Console.WriteLine("---------------------------------------");
+var homem = new Humano(2, "Ricardão", 35, "anos");
+Console.WriteLine(homem.Reproduzir());
+Console.WriteLine(homem.Morrer());
